a45e0ebaa8666f08c359f48d221083d2285cf169
amtoaer <amtoaer@outlook.com>
2020-09-28 22:27:39 +0800

    day 9.28

---
+++ 189.旋转数组.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=java
+ *
+ * [189] 旋转数组
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = result[i];
+        }
+    }
+}
+// @lc code=end
